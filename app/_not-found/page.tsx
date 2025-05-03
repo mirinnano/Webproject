6c1e37c003546ae43d072ebc8dc6d814afb9041b
mirinnano <yozume_jp@yahoo.co.jp>
2025-05-04 05:34:44 +0900

    no message

---
+++ app/_not-found/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from 'react';
+
+
+export default function NotFound() {
+  // useState などクライアントフックが使えます
+  return <h1>ページが見つかりませんでした</h1>;
+}
--- app/discordRPC/route.ts
+++ app/discordRPC/route.ts
@@ -1,4 +1,5 @@
 // app/discordRPC/route.ts
+
 export const runtime = 'nodejs'
 
 import { Client, PresenceData} from 'discord.js-selfbot-v13';
