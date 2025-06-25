de659a3cd7a7e5f68100ef385ad05cdd0925ab58
mirinnano <yozume_jp@yahoo.co.jp>
2025-06-25 14:21:33 +0900

    shine

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'https://blog.tokwiakano.uk',
+    siteUrl: 'https://romeda.tokwiakano.uk',
     generateRobotsTxt: true, // robots.txtの生成を有効に
     changefreq: 'daily',
     priority: 0.7,
